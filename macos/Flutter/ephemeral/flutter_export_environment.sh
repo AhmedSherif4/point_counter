8eeb71d7ba9188b2c7712396f7f1d716bab09dba
Ahmed Sherif <ahmedmesho853@gmail.com>
2023-01-21 15:35:04 +0200

    initial screen

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\Coding\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\Coding\Projects\1Flutter code\Full projects wanna retake then rebuild from scrash\point_counter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
